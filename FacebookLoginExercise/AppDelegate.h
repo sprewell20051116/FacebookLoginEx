62c9d0b7eae6a7b937c20b9635473a39a306e680
GIGIGUN <sprewell20051116@gmail.com>
2016-10-19 19:56:27 +0800

    Initial Commit

---
+++ FacebookLoginExercise/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  FacebookLoginExercise
+//
+//  Created by GIGIGUN on 19/10/2016.
+//  Copyright © 2016 GIGIGUN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
